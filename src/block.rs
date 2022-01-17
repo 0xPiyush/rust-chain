5309067fe53e9c30aaa51ba95cc685ff871f0449
BuildTools <unconfigured@null.spigotmc.org>
2022-01-17 18:01:58 +0530

    Added block hashing

--- src/block.rs
+++ src/block.rs
@@ -1,12 +1,14 @@
 use std::fmt::{self, Debug, Formatter};
 
-use super::BlockHash;
+use crate::hashable::Hashable;
+
+use super::*;
 
 pub struct Block {
     pub index: u32,
     pub timestamp: u128,
-    pub hash: BlockHash,
-    pub previous_hash: BlockHash,
+    pub hash: Hash,
+    pub previous_hash: Hash,
     pub nonce: u64,
     pub payload: String,
 }
@@ -28,7 +30,7 @@ impl Block {
     pub fn new(
         index: u32,
         timestamp: u128,
-        previous_hash: BlockHash,
+        previous_hash: Hash,
         nonce: u64,
         payload: String,
     ) -> Block {
@@ -40,5 +42,18 @@ impl Block {
             nonce,
             payload,
         }
+    }
+}
+
+impl Hashable for Block {
+    fn bytes(&self) -> Vec<u8> {
+        let mut bytes = vec![];
+        bytes.extend(&u32_bytes(&self.index));
+        bytes.extend(&u128_bytes(&self.timestamp));
+        bytes.extend(&self.previous_hash);
+        bytes.extend(&u64_bytes(&self.nonce));
+        bytes.extend(&*self.payload.as_bytes());
+
+        bytes
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
-use rust_chain::block::Block;
+use rust_chain::{block::Block, hashable::Hashable};
 
 fn main() {
     let block = Block::new(0, 0, vec![0; 32], 0, "Genesis block".to_owned());
-    println!("{:?}", block);
+    println!("{:?}", block.hash());
 }
